50a0f6c7c4c578defef57760df11355fa8728d13
Benson Ibeabuchi <ibeabuchibenson@gmail.com>
2024-12-18 23:37:32 -0500

    added my name

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Welcome To Benson Plant Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
          
           <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
+            Lets Go
           </button>
          </div>
           <div className="aboutus_container">
